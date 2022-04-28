d532d502f759f15492b4800fb9b198d9f1352489
Harry <hareshwarp649>
2022-04-28 11:05:29 +0530

    [HRPatil]Added: Harmonic Problem

---
+++ w2/workshop2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workshop2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int k, i;
+            float G = 0;
+            Console.WriteLine("Enter the Number");
+            k = Convert.ToInt32(Console.ReadLine());
+
+            for( i=1; i< k; i++)
+            {
+                G += 1 / (float)i;
+
+            }
+            Console.WriteLine("Harmonic Number :" + G);
+        }
+    }
+}
